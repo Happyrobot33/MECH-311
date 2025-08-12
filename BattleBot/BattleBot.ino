6feab83ea542c559b331ab1abcd7ff0bf0c5ab00
Matthew Herber <Matthewherber@sbcglobal.net>
2025-08-12 15:19:59 -0400

    initial battlebot code

---
+++ BattleBot/BattleBot.ino
@@ -0,0 +1,112 @@
+#include <SparkFun_TB6612.h>
+
+const int LEFT_MOTOR_IN1 = 4;
+const int LEFT_MOTOR_IN2 = 5;
+const int LEFT_MOTOR_PWM = 3;
+const int LEFT_MOTOR_OFFSET = 1; // lets me flip direction if I wired wrong
+
+const int RIGHT_MOTOR_IN1 = 7;
+const int RIGHT_MOTOR_IN2 = 8;
+const int RIGHT_MOTOR_PWM = 6;
+const int RIGHT_MOTOR_OFFSET = 1; // lets me flip direction if I wired wrong
+
+const int STBY = 9;
+
+Motor leftMotor(LEFT_MOTOR_IN1, LEFT_MOTOR_IN2, LEFT_MOTOR_PWM, LEFT_MOTOR_OFFSET, STBY);
+Motor rightMotor(RIGHT_MOTOR_IN1, RIGHT_MOTOR_IN2, RIGHT_MOTOR_PWM, RIGHT_MOTOR_OFFSET, STBY);
+
+const int LIGHT_SENSOR_PIN = A0; // Analog pin for light sensor
+
+void setup()
+{
+    Serial.begin(115200);
+
+    //setup light sensor
+    pinMode(LIGHT_SENSOR_PIN, INPUT);
+}
+
+void loop()
+{
+    //read from the light sensor
+    int lightValue = analogRead(LIGHT_SENSOR_PIN);
+    Serial.print("Light Sensor Value: ");
+    Serial.println(lightValue);
+}
+
+
+
+
+
+
+//util functions
+
+// Turn by a specified ammount of degrees either positive or negative by a 0 to 100 pct
+void Turn(int degrees, int speedPct)
+{
+// hand tuned scalar to make the degrees match with the delay
+#define degreesScalar 265
+    int timeDelay = (degreesScalar * abs(degrees)) * (1.0 / speedPct);
+    if (degrees > 0)
+    {
+        Right(speedPct);
+    }
+    else
+    {
+        Left(speedPct);
+    }
+    delay(timeDelay);
+    Stop();
+}
+
+void Drive(int speedPct)
+{
+    if (speedPct > 0)
+    {
+        Forward(speedPct);
+    }
+    else
+    {
+        Backwards(abs(speedPct));
+    }
+}
+
+// General movement commands with a percentage
+void Forward(int speedPct)
+{
+    rightMotor.drive(speedPct);
+    leftMotor.drive(speedPct);
+}
+
+void Backwards(int speedPct)
+{
+    rightMotor.drive(-speedPct);
+    leftMotor.drive(-speedPct);
+}
+
+void Right(int speedPct)
+{
+    rightMotor.drive(-speedPct);
+    leftMotor.drive(speedPct);
+}
+
+void Left(int speedPct)
+{
+    rightMotor.drive(speedPct);
+    leftMotor.drive(-speedPct);
+}
+
+// Drives the robot forward with a differential between both wheels
+// Positive is right, negative is left
+void DifferentialForward(int speedPct, int ratio)
+{
+    rightMotor.drive(speedPct - ratio);
+    leftMotor.drive(speedPct + ratio);
+}
+
+// stop both motors, with a delay afterwards to prevent drifting
+void Stop()
+{
+    rightMotor.brake();
+    leftMotor.brake();
+    delay(500); // wait half a second to avoid drifting
+}
